fca91731a6a97d939d3450e8b91e3061ed73f4fc
Martin Rohwedder <m.rohwedder@gmail.com>
2020-11-03 22:39:09 +0100

    Initial commit

---
+++ PokerBlindsMobile/PokerBlindsMobile/ViewModels/PlayPokerViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace PokerBlindsMobile.ViewModels
+{
+    public class PlayPokerViewModel : BaseViewModel
+    {
+        private TimeSpan _currentTime;
+        public TimeSpan CurrentTime 
+        { 
+            get { return _currentTime; }
+            set 
+            {
+                if (TimeSpan.Compare(value, TimeSpan.Zero) < 0)
+                {
+                    SetValue(ref _currentTime, new TimeSpan(0, 1, 0));
+                }
+                else
+                {
+                    SetValue(ref _currentTime, value);
+                }
+            }
+        }
+
+        private bool _timerToggle;
+        public bool TimerToggle { 
+            get { return _timerToggle; } 
+            set { SetValue(ref _timerToggle, value); }
+        }
+
+        public PlayPokerViewModel()
+        {
+            CurrentTime = new TimeSpan(0, 0, 10);
+            TimerToggle = false;
+        }
+
+        public ICommand StartStopTimerCommand { get { return new Command<bool>((param) => StartStopTimer(param)); } }
+        private void StartStopTimer(bool shouldStart)
+        {
+            if (shouldStart)
+            {
+                TimerToggle = shouldStart;
+
+                var second = TimeSpan.FromSeconds(1);
+                Device.StartTimer(second, () => {
+                    CurrentTime = CurrentTime.Subtract(second);
+                    return TimerToggle;
+                });
+            }
+            else
+            {
+                TimerToggle = shouldStart;
+            }
+
+            Debug.WriteLine($"Toggle is: {shouldStart}");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Poker Blinds Mobile
+ 
